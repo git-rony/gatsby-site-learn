da92efcb36fc01779346b419dcd27bb8388e210a
git-rony <git-rony@devxhub.com>
2024-11-07 20:27:49 +0600

    add image using plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,7 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
-module.exports = {
-  siteMetadata: {
-    title: `My Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }]
-};
+
 
 module.exports = {
   siteMetadata: {
@@ -24,4 +11,8 @@ module.exports = {
     image: `/gatsby-icon.png`,
     siteUrl: `https://learn-gastby.netlify.app`,
   },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { StaticImage } from 'gatsby-plugin-image';
 import * as React from 'react';
 import LayOut from '../components/layout';
 const IndexPage = ()=> {
@@ -5,6 +6,10 @@ const IndexPage = ()=> {
     <main>
       <LayOut pageTitle={'WelCome to my GatsBy Site !'}>
       <p className='text-xl font-medium'>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
       </LayOut>
     </main>
   )
